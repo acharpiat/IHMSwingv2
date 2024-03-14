ad772ad7f83ed5cfb56cd6baf99fd518b07bfef8
Arthur Charpiat <arthur.charpiat@gmail.com>
2024-03-14 10:57:07 +0100

    added final images

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,23 @@
+
+d
+4src/main/java/com/ubo/tp/message/ihm/MessageApp.java,0\9\094e6011c8e2419cdbbf32f42d16621c154637b3
+r
+Bsrc/main/java/com/ubo/tp/message/ihm/message/MessageComponent.java,e\9\e9b474d7186efc6ee80867b945908109ab559ca1
+;
+Jenkinsfile,0\c\0c2c5c506cef0ed2d3ad2b7e45e97980b66f5520
+|
+Lsrc/main/java/com/ubo/tp/message/datamodel/jaxb/bean/message/MessageXml.java,4\3\43875e5ec7d5796d33927f0e3a48a4acb7a63bdb
+~
+Nsrc/main/java/com/ubo/tp/message/datamodel/jaxb/bean/message/package-info.java,f\6\f6f45143169c4999ea2f47fa8fe91e48e8c309be
+o
+?src/main/java/com/ubo/tp/message/datamodel/jaxb/JaxbReader.java,f\f\ffb25a703207e90f3342e27e9bf182471231a85f
+i
+9src/main/java/com/ubo/tp/message/ihm/session/Session.java,c\e\ce547f9ed54a9fb10da410a8d19ad93a978aa853
+s
+Csrc/main/java/com/ubo/tp/message/ihm/session/SessionController.java,3\4\34767c962a13db9c438e43aac85dece5c0d0b8e2
+l
+<src/main/java/com/ubo/tp/message/ihm/MessageAppMainView.java,3\f\3ff5278ea26a17965c823e9693af378c2445231e
+q
+Asrc/main/java/com/ubo/tp/message/ihm/signin/SignInController.java,3\e\3e8ebf1442f41f2401fb04594145a40b6a9c0692
+9
+	build.xml,b\d\bdc348bf75801c527f51cdeddc8edf027f7ef35a
--- src/main/java/com/ubo/tp/message/ihm/message/MessageComponent.java
+++ src/main/java/com/ubo/tp/message/ihm/message/MessageComponent.java
@@ -16,15 +16,20 @@ public class MessageComponent extends JPanel {
             message.getSender().setAvatarPath("D:\\M2\\IHM\\LUCAS\\MessageApp\\src\\main\\resources\\images\\img_2.png");
         }
         File file = new File(message.getSender().getAvatarPath());
-        ImagePanel imagePanel = new ImagePanel(file, new Dimension(80, 100));
+        ImagePanel imagePanel = new ImagePanel(file, new Dimension(80, 80));
         m = new JLabel(message.getText());
+        JLabel sender = new JLabel(message.getSender().getUserTag() + " dit :");
         JPanel customPanel = new JPanel();
-        customPanel.add(m);
-        customPanel.add(imagePanel);
         customPanel.setLayout(new GridBagLayout());
+        customPanel.add(imagePanel, new GridBagConstraints(0, 0, 1, 2, 1, 1, GridBagConstraints.CENTER,
+                GridBagConstraints.BOTH, new Insets(5, 5, 5, 5), 0, 0));
+        customPanel.add(sender, new GridBagConstraints(1, 0, 1, 1, 1, 0, GridBagConstraints.WEST,
+                GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5, 5), 0, 0));
+        customPanel.add(m, new GridBagConstraints(1, 1, 1, 1, 1, 0, GridBagConstraints.WEST,
+                GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5, 5), 0, 0));
         setLayout(new GridBagLayout());
         add(customPanel, new GridBagConstraints(0, 0, 1, 1, 1, 1, GridBagConstraints.CENTER,
-                GridBagConstraints.BOTH, new Insets(5, 5, 5, 5), 0, 0));
+                GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5, 5), 0, 0));
         setBackground(Color.lightGray);
         add(m);
 
